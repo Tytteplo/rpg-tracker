bffd2e4360869a6bbe8d7e5c6b30a6fba074991a
Tytteplo <zhenechka.teplova@mail.ru>
2026-09-17 12:02:14 +1000

    Add icons to APP_SHELL in service worker

--- sw.js
+++ sw.js
@@ -1,14 +1,11 @@
 const CACHE = 'rpg-tracker-v3';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
-
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon-180.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', event => {
   const req = event.request;
   if (req.method !== 'GET') return;
